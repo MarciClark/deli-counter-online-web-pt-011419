2f64097b0e46895041a1ccf9206df4a110663932
Marci Clark <marcig612@gmail.com>
2019-01-23 02:51:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 take_a_number(katz_deli, "Ada")
 take_a_number(katz_deli, "Grace")
 take_a_number(katz_deli, "Kent")
-line(katz_deli)
+customers(katz_deli)
 now_serving(katz_deli)
 line(katz_deli)
 end
